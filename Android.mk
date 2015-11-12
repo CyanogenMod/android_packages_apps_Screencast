3d3ecd72b51d135c91c7359d2294956dfc240d4f
Ricardo Cerqueira <ricardo@cyngn.com>
2015-11-12 13:55:38 +0000

    Switch to CyanogenMod namespace

--- Android.mk
+++ Android.mk
@@ -14,9 +14,4 @@ LOCAL_PACKAGE_NAME := Screencast
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PRIVILEGED_MODULE := true
 
-# Sign the package when not using test-keys
-ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/target/product/security/testkey)
-LOCAL_CERTIFICATE := cyngn-app
-endif
-
 include $(BUILD_PACKAGE)
