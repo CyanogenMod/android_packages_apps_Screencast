7c8be344ea0b73dcd4c69fc42fe78c44ad7087dc
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-31 20:32:02 +0100

    Screencast: material makeover
    Icon by Jovie
    
    Change-Id: I11a3eafd2c2d67b7e263e559637aee8bc8ddb4b3
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- src/org/cyanogenmod/screencast/MainActivity.java
+++ src/org/cyanogenmod/screencast/MainActivity.java
@@ -17,6 +17,7 @@
 package org.cyanogenmod.screencast;
 
 import android.Manifest;
+import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,88 +25,58 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    Button mStartScreencastButton;
-    Button mStopScreencastButton;
-    Button mSettingsButton;
-    TextView mStartDescription;
-    View mNoAudioWarning;
-
-    boolean mHasAudioPermission = false;
-
+    private ImageButton mScreencastButton;
+    private TextView mText;
+    private TextView mAudioText;
+    private boolean mHasAudioPermission;
     private static final int REQUEST_AUDIO_PERMS = 654;
-
-    private boolean hasPermissions() {
-        int res = checkCallingOrSelfPermission(Manifest.permission.RECORD_AUDIO);
-        return (res == PackageManager.PERMISSION_GRANTED);
-    }
-
-    private void requestNecessaryPermissions() {
-        String[] permissions = new String[] {
-                Manifest.permission.RECORD_AUDIO,
-        };
-        requestPermissions(permissions, REQUEST_AUDIO_PERMS);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        if (requestCode == REQUEST_AUDIO_PERMS) {
-            mHasAudioPermission = true;
-            for (int res : grantResults) {
-                mHasAudioPermission &= (res == PackageManager.PERMISSION_GRANTED);
-            }
-            refreshState();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        if (!hasPermissions()) {
-            requestNecessaryPermissions();
-        } else {
-            mHasAudioPermission = true;
-        }
+        mScreencastButton = (ImageButton) findViewById(R.id.screencast);
+        mText = (TextView) findViewById(R.id.hint);
+        mAudioText = (TextView) findViewById(R.id.audio_warning);
 
-        mStartDescription = (TextView) findViewById(R.id.start_description);
-        mNoAudioWarning = findViewById(R.id.no_audio_warning);
-
-        mStartScreencastButton = (Button) findViewById(R.id.start_screencast);
-        mStartScreencastButton.setOnClickListener(new View.OnClickListener() {
+        mScreencastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getSharedPreferences(ScreencastService.PREFS, 0).edit().putBoolean(ScreencastService.KEY_RECORDING, true).apply();
-                startService(new Intent("org.cyanogenmod.ACTION_START_SCREENCAST")
-                        .setClass(MainActivity.this, ScreencastService.class));
-                finish();
+                animateFAB(v);
+                if (getSharedPreferences(ScreencastService.PREFS, 0).getBoolean(ScreencastService.KEY_RECORDING, false)) {
+                    // stop
+                    getSharedPreferences(ScreencastService.PREFS, 0).edit().putBoolean(ScreencastService.KEY_RECORDING, false).apply();
+                    startService(new Intent("org.cyanogenmod.ACTION_STOP_SCREENCAST")
+                            .setClass(MainActivity.this, ScreencastService.class));
+                    refreshState();
+                } else {
+                    // record
+                    getSharedPreferences(ScreencastService.PREFS, 0).edit().putBoolean(ScreencastService.KEY_RECORDING, true).apply();
+                    startService(new Intent("org.cyanogenmod.ACTION_START_SCREENCAST")
+                            .setClass(MainActivity.this, ScreencastService.class));
+                    finish();
+                }
             }
         });
 
-        mStopScreencastButton = (Button) findViewById(R.id.stop_screencast);
-        mStopScreencastButton.setOnClickListener(new View.OnClickListener() {
+        mAudioText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getSharedPreferences(ScreencastService.PREFS, 0).edit().putBoolean(ScreencastService.KEY_RECORDING, false).apply();
-                startService(new Intent("org.cyanogenmod.ACTION_STOP_SCREENCAST")
-                        .setClass(MainActivity.this, ScreencastService.class));
-                refreshState();
+                requestNecessaryPermissions();
             }
         });
 
-        mSettingsButton = (Button) findViewById(R.id.settings);
-        mSettingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToSettings();
-            }
-        });
+        refreshState();
     }
 
     @Override
@@ -121,30 +92,43 @@ public class MainActivity extends Activity {
         refreshState();
     }
 
+    private boolean hasPermissions() {
+        int res = checkSelfPermission(Manifest.permission.RECORD_AUDIO);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestNecessaryPermissions() {
+        String[] permissions = new String[] {
+            Manifest.permission.RECORD_AUDIO,
+        };
+        requestPermissions(permissions, REQUEST_AUDIO_PERMS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                            int[] grantResults) {
+        if (requestCode == REQUEST_AUDIO_PERMS) {
+            for (int res : grantResults) {
+                mHasAudioPermission &= (res == PackageManager.PERMISSION_GRANTED);
+            }
+            refreshState();
+        }
+    }
+
     private void refreshState() {
         final boolean recording = getSharedPreferences(ScreencastService.PREFS, 0).getBoolean(ScreencastService.KEY_RECORDING, false);
-        if (mStartScreencastButton != null) {
-            mStartScreencastButton.setEnabled(!recording);
-        }
-        if (mStopScreencastButton != null) {
-            mStopScreencastButton.setEnabled(recording);
-        }
-
-        int visibility = mHasAudioPermission ? View.GONE : View.VISIBLE;
-        if (mSettingsButton != null) {
-            mSettingsButton.setVisibility(visibility);
-        }
-        if (mNoAudioWarning != null) {
-            mNoAudioWarning.setVisibility(visibility);
+        if (recording) {
+            mScreencastButton.setImageResource(R.drawable.stop);
+            mText.setText(R.string.stop_description);
+            mAudioText.setVisibility(View.GONE);
+        } else {
+            mScreencastButton.setImageResource(R.drawable.record);
+            mText.setText(mHasAudioPermission ?
+                R.string.start_description : R.string.start_description_no_audio);
+            mAudioText.setVisibility(mHasAudioPermission ?
+                View.GONE: View.VISIBLE);
         }
 
-        if (mStartDescription != null) {
-            mStartDescription.setText(
-                mHasAudioPermission
-                    ? R.string.start_description
-                    : R.string.start_description_no_audio
-            );
-        }
     }
 
     private void goToSettings() {
@@ -156,4 +140,19 @@ public class MainActivity extends Activity {
         intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         startActivity(intent);
     }
+
+    private void animateFAB(View view) {
+        int centerX = (view.getLeft() + view.getRight()) / 2;
+        int centerY = (view.getTop() + view.getBottom()) / 2;
+        int startRadius = 0;
+        int endRadius =
+            (int) Math.hypot(view.getWidth(), view.getHeight());
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(
+            view, centerX, centerY, startRadius, endRadius);
+
+        anim.setDuration(800);
+        anim.start();
+    }
+
 }
